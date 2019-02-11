bba7442276a7162c50a629e57abf61c6fec96a8d
Roberto Mariani <roberto.mariani@jelot.it>
2019-02-11 03:05:01 +0100

    Initial work on 'JGAurora A5S A1' board

---
+++ Marlin/src/pins/pins_JGAURORA_A5S_A1.h
@@ -0,0 +1,90 @@
+/**
+ * Marlin 3D Printer Firmware
+ * Copyright (C) 2016 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
+ *
+ * Based on Sprinter and grbl.
+ * Copyright (C) 2011 Camiel Gubbels / Erik van der Zalm
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+ /**
+  * 2017 Victor Perez Marlin for stm32f1 test
+  * 2018 Modified by Pablo Crespo for Morpheus Board (https://github.com/pscrespo/Morpheus-STM32)
+  * 2019 Modified by Roberto Mariani for JGAurora A5S A1 Board.
+  */
+
+/**
+ * JGAurora A5S A1 Board pin assignments
+ */
+
+#ifndef __STM32F1__
+  #error "Oops! Select an STM32F1 board in 'Tools > Board.'"
+#endif
+
+#define BOARD_NAME "JGAurora A5S A1 board"
+
+//
+// Limit Switches
+//
+#define X_MIN_PIN          PC6
+#define X_MAX_PIN          -1
+#define Y_MIN_PIN          PG8
+#define Y_MAX_PIN          -1
+#define Z_MIN_PIN          PG7
+#define Z_MAX_PIN          -1
+
+//
+// Steppers
+//
+#define X_STEP_PIN         PD6
+#define X_DIR_PIN          PD3
+#define X_ENABLE_PIN       PG9
+
+#define Y_STEP_PIN         PG12
+#define Y_DIR_PIN          PG11
+#define Y_ENABLE_PIN       PG13
+
+#define Z_STEP_PIN         PG15
+#define Z_DIR_PIN          PG14
+#define Z_ENABLE_PIN       PB8
+
+#define E0_STEP_PIN        PE2
+#define E0_DIR_PIN         PB9
+#define E0_ENABLE_PIN      PE3
+
+#define E1_STEP_PIN        PE5
+#define E1_DIR_PIN         PE4
+#define E1_ENABLE_PIN      PE6
+
+//
+// Temperature Sensors
+//
+#define TEMP_0_PIN         PC2
+#define TEMP_BED_PIN       PC1
+
+//
+// Heaters / Fans
+//
+#define HEATER_0_PIN       PA2
+#define HEATER_BED_PIN     PA3
+
+#define FAN_PIN            PA1
+
+//
+// Misc.
+//
+#define LED_PIN            -1
+#define SDSS               -1
